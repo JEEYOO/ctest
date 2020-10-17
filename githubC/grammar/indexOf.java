c9f9a66ba701a82513162f564835fb3950a67213
Jee Yoo <yoojw94@gmail.com>
2020-10-17 10:11:03 +0900

    Add files via upload

---
+++ githubC/grammar/indexOf.java
@@ -0,0 +1,19 @@
+package grammar;
+
+public class indexOf {
+	public static void main(String[] args) {
+		String what = "abcde";
+		
+		System.out.println(what.indexOf("c"));
+		
+		String whatever = "12345";
+		System.out.println(what.indexOf('3'));
+		
+		String whattever = "   s asdf a sd 1 2 ";
+		String trimone = whattever.trim();
+		System.out.println(whattever);
+		System.out.println(trimone);
+		
+		
+	}
+}
