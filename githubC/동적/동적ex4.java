f2dc98831cb1b6d1102cfa5f5acb2f84106e9560
Jee Yoo <yoojw94@gmail.com>
2020-11-05 15:25:17 +0900

    Add files via upload

---
+++ githubC/동적/동적ex4.java
@@ -0,0 +1,24 @@
+package 동적;
+
+public class 동적ex4 {// 백준 11727 행렬 2xn 
+	
+	public static int dynamics(int a) {
+		if (a==1) {
+			return 1;
+		} else if(a==2) {
+			return 3;
+		}
+		
+		int result =0;
+		result += dynamics(a-1);
+		result += dynamics(a-2)*2; // 사실 3이라해도되긴 
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(dynamics(8));
+		//.......... 어떻게하다보니되었네?
+	}
+
+}
