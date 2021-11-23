71b51e6bab6f066b65f635a7b385052e692c7413
Jee Yoo <yoojw94@gmail.com>
2021-11-23 23:36:30 +0900

    Update chuseok.py

--- githubC/programmersKakao/chuseok.py
+++ githubC/programmersKakao/chuseok.py
@@ -1,4 +1,11 @@
 def solution(lines):
     answer = 0
-  
+    
+    
+    for num in range(len(lines)):
+    
+        each = lines[num].split()
+        time = each[1].split(':')
+    
+    
     return answer
