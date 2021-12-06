6b292b069b2c1fa6518a213014450f7f9014190a
Jee Yoo <yoojw94@gmail.com>
2021-12-06 23:38:23 +0900

    Create representbyN.py

---
+++ githubC/programmersKakao/representbyN.py
@@ -0,0 +1,17 @@
+#count = 0
+
+possibleList = [1] 
+
+def arithmetic(N, List) :
+    if N + N not in possibleList :
+        possibleList.append(N+N)
+    if N * N not in possibleList :
+        possibleList.append(N*N)
+
+def solution(N, number):
+    count = 0
+    
+   
+    possibleList
+    
+    return answer
