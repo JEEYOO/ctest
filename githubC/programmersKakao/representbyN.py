256c8d5d7e36b187398cf197461f46a7223e5261
Jee Yoo <yoojw94@gmail.com>
2021-12-17 21:07:42 +0900

    Update representbyN.py

--- githubC/programmersKakao/representbyN.py
+++ githubC/programmersKakao/representbyN.py
@@ -7,14 +7,14 @@ def arithmetic(N, List) :
 def solution(N, number):
     possibleList
     possibleList.append([])
-    a[count].append(N+N)
-    a[count].append(N-N) #0
-    a[count].append(N*N) 
-    a[count].append(N/N) #1 
+    possibleList[count].append(N+N)
+    possibleList[count].append(N-N) #0
+    possibleList[count].append(N*N) 
+    possibleList[count].append(N/N) #1 
     
-    while (count<=7){ #0부터 7까지. 배열넣으려면 0부터시작하는게 나음
-        arithmetic(count, N)
+    while (count<=7) : {  #0부터 7까지. 배열넣으려면 0부터시작하는게 나음
         count++
+        arithmetic(count, N)
     }
     
     
