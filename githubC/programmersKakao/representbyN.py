24998b0c6b7e50d6d91152ef6ba937e76f423cc9
Jee Yoo <yoojw94@gmail.com>
2021-12-29 01:10:39 +0900

    Update representbyN.py

--- githubC/programmersKakao/representbyN.py
+++ githubC/programmersKakao/representbyN.py
@@ -1,5 +1,5 @@
 
-possibleList = []
+
 
 def NN(N, times) :
     result = 0
@@ -10,40 +10,35 @@ def NN(N, times) :
         result += each
     return result
 
-def arithmetic(list1, list2, count) :
-    j= len(list1)
-    k= len(list2)
+def arithmetic(possibleListset, set1, set2, count,N) :
+    j= len(set1)
+    k= len(set2)
     
-    for a in range(j):
-        for b in range(k):
-            if (list1[a]+list2[b]) not in possibleList[count]:
-                possibleList[count].append(list1[a]+list2[b])
-            if (list1[a]-list2[b]) not in possibleList[count]:
-                possibleList[count].append(list1[a]-list2[b])
-            if (list1[a]*list2[b]) not in possibleList[count]:
-                possibleList[count].append(list1[a]*list2[b])
-            if (list2[b]!=0) : 
-                if (list1[a]/list2[b]) not in possibleList[count]:
-                    possibleList[count].append(list1[a]/list2[b])
+    for c in set1:
+        for d in set2:
+            possibleListset.add(c+d)
+            possibleListset.add(c-d)
+            possibleListset.add(c*d)
+            if (d!=0) : 
+                possibleListset.add(c//d)
+    return possibleListset
             
 def solution(N, number):
-    possibleList.append([]) #그냥 0은 empty하게 두자 
+    possibleList = []
+    possibleList.append(set())  #그냥 0은 empty하게 두자 
     
     count = 1
     if number == N : return count #N이면 그냥 1 제출하라는거지
     
-    possibleList.append([])
-    possibleList[count].append(N) #[ [], [5] ]
+    possibleList.append(set())
+    possibleList[count].add(N) #[ [], [5] ]
     # print(possibleList) 맞는데
-    while count <= 8:
+    while count < 8:
         count=count+1
-        possibleList.append([])
-        possibleList[count].append(NN(N, count)) #55 추가  
-        for i in range(1,count): 
-            #if (count-i) >= i:
-            arithmetic(possibleList[i], possibleList[count-i],count)
-            if number in possibleList[count]: 
-            #print(possibleList[count])
-            return count
+        possibleList.append(set()) # [ (), (5), (55,7897987) ]
+        possibleList[count].add(NN(N, count))
+        for i in range(1,count): #1,3
+            #if (count-i) >= i:              #1과 2       2와1
+            possibleList[count] = arithmetic(possibleList[count], possibleList[i], possibleList[count-i],count,N)
     
     return -1
