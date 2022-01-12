5210bd24ffba06eed1395cfb5e51c8c25ee703ce
Jee Yoo <yoojw94@gmail.com>
2022-01-12 23:59:52 +0900

    Update news_clustering.py

--- githubC/programmersKakao/news_clustering.py
+++ githubC/programmersKakao/news_clustering.py
@@ -2,7 +2,7 @@ def solution(str1, str2):
     answer = 0
     #원소중복허용하니까 set은 안됨 ascending 등 사용가능 <> 근데 순서는 안 중요함
     
-    list1, list2 = list(str1), list(str2)
+    list1, list2 = list(str1.lower()), list(str2.lower())
     print(list1) # sorted(list(str1)) Test 4 오류
     print(list2)
     print('e'=='E') #case sensative
