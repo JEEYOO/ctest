6a1d90612cee2546dd9b996a1e2c9b39fe69429d
Jee Yoo <yoojw94@gmail.com>
2022-01-16 23:02:01 +0900

    Update news_clustering.py

--- githubC/programmersKakao/news_clustering.py
+++ githubC/programmersKakao/news_clustering.py
@@ -1,23 +1,37 @@
 def combine(ori_list):
     out_list = []
-    if (len(ori_list)%2==0): #짝수라면
-        for i in range(len(ori_list)/2): #예를들어 4라면 2까지 
-            out_list.append(ori_list[2i]+ori_list[2i+1])
-    else:
-        for i in range(int(len(ori_list)/2)): #예를들어 3라면 1까지 
-            out_list.append(ori_list[2i]+ori_list[2i+1])
+    for i in range(len(ori_list)-1):
+        if (charnum(ori_list[i]) and charnum(ori_list[i+1])) :
+            if not (ori_list[i]+ori_list[i+1]).isnumeric():
+                out_list.append(ori_list[i]+ori_list[i+1])
     return out_list
+
+def charnum(subject):
+    if subject.isnumeric() or subject.isalpha():
+        return True
+    return False
 
 def solution(str1, str2):
     answer = 0
     #원소중복허용하니까 set은 안됨 ascending 등 사용가능 <> 근데 순서는 안 중요함
     
     list1, list2 = list(str1.lower()), list(str2.lower())
-    print(list1) # sorted(list(str1)) Test 4 오류
-    print(list2)
-    print('e'=='E') #case sensative
+    list1, list2 = combine(list1), combine(list2)
+        
+    numerator = set()
+    denominator = set()
+    for a in list1:
+        denominator.add(a)
+    for b in list2:
+        denominator.add(b)
     
-    return answer
+    for k in range(len(list1)): #더 짧은 list로 해야
+        if list1[k] in list2: #set으로해야겠는데 
+            numerator.add(list1[k])
+    print(denominator)            
+    if len(numerator) == len(list1) : return 65536
+    
+    return int(65536*(len(numerator)/len(denominator)))
 
 '''
 list.sort() = none
