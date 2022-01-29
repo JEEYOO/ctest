8bfcd7f0084c8820a0ed12fed11de79ff074d028
Jee Yoo <yoojw94@gmail.com>
2022-01-29 16:12:33 +0900

    Create receive_report.py

---
+++ githubC/programmersKakao/receive_report.py
@@ -0,0 +1,10 @@
+def solution(id_list, report, k):
+    answer = []
+    point = [] #two list for report and result
+    report_dic = {}
+    #dictionary
+    for each in report:
+        if each.split()[0] in report_dic:
+            each.split()[0]
+    
+    return answer
