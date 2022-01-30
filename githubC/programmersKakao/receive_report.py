2d676d347e6f7b9c6d51b4c4e94bb979ca2607b3
Jee Yoo <yoojw94@gmail.com>
2022-01-30 22:41:27 +0900

    Update receive_report.py

--- githubC/programmersKakao/receive_report.py
+++ githubC/programmersKakao/receive_report.py
@@ -2,7 +2,10 @@ def solution(id_list, report, k):
     answer = []
     point = [] #two list for report and result
     report_dic = {}
-    #dictionary
+    
+    for each_id in id_list:
+        report_dic[each_id] = 0
+    
     for each in report:
         if each.split()[0] in report_dic:
             each.split()[0]
