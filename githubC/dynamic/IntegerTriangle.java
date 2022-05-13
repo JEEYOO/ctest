e486ee69d3ee97aabdb9a18ac770269e92cc06cc
Jee Yoo <yoojw94@gmail.com>
2022-05-13 23:31:50 +0900

    Update IntegerTriangle.java

--- githubC/dynamic/IntegerTriangle.java
+++ githubC/dynamic/IntegerTriangle.java
@@ -32,33 +32,3 @@ public class IntegerTriangle {
     
 }
 
-/*
-<> 실험하는 거는 좋은데 이 경우는 아예 bound error 나버려서 내가 미리조치해야함 
-
-public int dynamics(int[][] triangle, int floor) {
-       
-   }
-*/
-
-/*
- * public int solution(int[][] triangle) {
-        int answer = 0;
-        //1번째 2번째까지만 한 다음에
-        int floor = triangle.size(); // 나중에 -1해야겠지 
-        // k k+1 로 하면 되겠는데 <> out of bounds도 안 걸릴거고 
-        
-        return answer;
-    }
-    
-    public int dynamics(int[][] triangle, int floor) {
-        if (floor ==1) { return triangle[0][0]; }
-        
-        int x=0;
-        while (x != floor) {
-            [그대로 x][+1]
-            x++;
-        }
-        //... 개념도 확실하지 않은데 담는 법을 어찌알겟누
-    }
- * */
- 
