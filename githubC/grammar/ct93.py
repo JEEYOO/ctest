d0874cebaad0f12382de687d96dcbf6d29c53447
Jee Yoo <yoojw94@gmail.com>
2025-02-01 16:29:40 +0900

    Update ct93.py

--- githubC/grammar/ct93.py
+++ githubC/grammar/ct93.py
@@ -4,3 +4,10 @@ print(t)
 #a, b, c = map(int, input().split())
 
 #numbers = list(map(int, input().split()))
+
+a = 4
+print("a")
+print('a')
+print(a)
+print(f"Number {a}")
+print('number',str(a),'test')
