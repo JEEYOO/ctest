765d3caeaab0f3b61cc8d2af364456bebe786bf6
Jee Yoo <yoojw94@gmail.com>
2025-05-18 16:48:16 +0900

    Update longestPalindromic.py

--- leetCode/longestPalindromic.py
+++ leetCode/longestPalindromic.py
@@ -4,24 +4,22 @@ class Solution:
         if len(s) <= 1 : return s
         
         answer = ""
-        #ODD
-        if len(s) % 2 == 1 :
-            answer = s[len(s)//2]
-            mid = len(s)//2 
-            for i in range(1, len(s)//2): #1AND 3
-                if s[mid-i] == s[mid+i] :
-                    answer += s[mid-i] #BACK
-                    answer = s[mid-i] + answer #FRONT
-                else : return answer                                
-        #EVEN  
-        else : 
-            mid1 = len(s)//2 - 1
-            mid2 = len(s)//2
-            answer = s[mid1:mid2+1]
-            for i in range(1, len(s)//2): #1AND 3
-                if s[mid1-i] == s[mid2+i] :
-                    answer += s[mid1-i] #BACK
-                    answer = s[mid1-i] + answer #FRONT 
-                else : return answer 
+        
+        for mid in range(len(s)):
+            # ODD PALINDROMIC
+            left, right = mid, mid
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(answer):
+                    answer = s[left:right+1]
+                left -= 1
+                right += 1
+
+            # EVEM PALINDROMIC
+            left, right = mid, mid + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(answer):
+                    answer = s[left:right+1]
+                left -= 1
+                right += 1
 
         return answer
