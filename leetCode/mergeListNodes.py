85caf0d6319cd087103abec02a1e9da67bf83bb0
Jee Yoo <yoojw94@gmail.com>
2025-05-28 20:21:27 +0900

    Create mergeListNodes.py

---
+++ leetCode/mergeListNodes.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+
+        answer : Optional[ListNode] = ListNode(0)
+        answer2 = answer
+
+        while list1 and list2 :
+            if list1.val < list2.val :
+                answer.next = ListNode(list1.val)
+                answer = answer.next
+                list1 = list1.next
+                
+            else :
+                answer.next = ListNode(list2.val)
+                answer = answer.next
+                list2 = list2.next
+
+        return answer2.next
+        
+        #return list1
+    
+    def leng(self, whatever) : 
+        count = 0
+        while whatever:
+            count+=1
+            whatever = whatever.next     
+        return count
+
+"""
+while self.leng(answer) != self.leng(list1)+self.leng(list2) :
+
+#last node
+while list1.next:
+    list1 = list1.next
+"""
