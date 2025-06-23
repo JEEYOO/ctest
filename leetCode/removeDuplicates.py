cab62b850970cb65b19f6b43f4f9ac3405b4815b
Jee Yoo <yoojw94@gmail.com>
2025-06-23 15:42:32 +0900

    Update removeDuplicates.py

--- leetCode/removeDuplicates.py
+++ leetCode/removeDuplicates.py
@@ -4,14 +4,13 @@ class Solution:
         setNums = set(nums)
         answer : List[int] = []
 
-        for i in setNums:
+        for i in sorted(setNums):
             answer.append(int(i))
 
-
-        for i in range (1, len(nums)):
-            for j in range (i+1, len(nums)): #2 -> indexOut 
-                 if nums[i] != nums[j] and nums[i] < nums[j]  : #and nums[i-1]+1 == nums[j]
-                    nums[i] = nums[j]
-                    break
+        
+        numbering = 0
+        for eachNum in answer:
+            nums[numbering] = eachNum
+            numbering += 1
 
         return len(answer)
