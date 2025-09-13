8efbba25698723c37d607c8cfcec3d0ae997952e
Jee Yoo <yoojw94@gmail.com>
2025-09-13 15:36:53 +0900

    Create sortedArrayToBST.py

---
+++ leetCode/sortedArrayToBST.py
@@ -0,0 +1,10 @@
+def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+
+        return root
