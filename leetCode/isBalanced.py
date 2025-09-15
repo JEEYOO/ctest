a9d159d1854e4cbb1203e818fdeb13e9af29a37f
Jee Yoo <yoojw94@gmail.com>
2025-09-15 17:33:23 +0900

    Update isBalanced.py

--- leetCode/isBalanced.py
+++ leetCode/isBalanced.py
@@ -1,26 +1,21 @@
-def isBalanced(self, root: Optional[TreeNode]) -> bool:
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
         if root is None :
             return True
 
-        left_depth = self.depthCheckL(root.left) 
-        right_depth = self.depthCheckR(root.right) 
-
-        # print(left_depth,right_depth)
-
-        if abs(left_depth - right_depth) <= 1 :
-            return True
+        if self.depthCheck(root) == -1 :
+            return False
         else : return False
 
-    def depthCheckL(self, root: Optional[TreeNode]) -> int:
+    def depthCheck(self, root: Optional[TreeNode]) -> int:
         if root is None :
             return 0
-        depthLL = self.depthCheckL(root.left) + 1
-        depthLR = self.depthCheckL(root.right) + 1
-        return max(depthLL, depthLR)
-
-    def depthCheckR(self, root: Optional[TreeNode]) -> int:
-        if root is None :
-            return 0
-        depthRL = self.depthCheckR(root.left) + 1
-        depthRR = self.depthCheckR(root.right) + 1
-        return max(depthRL, depthRR)
+        depthL = self.depthCheckL(root.left)
+        depthR = self.depthCheckL(root.right)
+        
+        if depthL == -1 or depthR == -1 :
+            return -1
+        elif abs(depthL - depthR) > 1 :
+            return -1
+        else :
+            return max(depthLL, depthLR) + 1
