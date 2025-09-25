7e87aef77bc3eb90d5a937c1ed23c52c1c0728a8
Jee Yoo <yoojw94@gmail.com>
2025-09-25 15:34:02 +0900

    Update generate.py

--- leetCode/generate.py
+++ leetCode/generate.py
@@ -1,23 +1,25 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
-        listlist = []
-        #List[List[int]]
-        for i in range(numRows) : 
-            tempList = []
-            '''
-            FIRST & LAST 1
-            '''
+        listlist = [[1]]
+        if numRows == 1 : return listlist
             
-            #1 FIRST 
-            tempList.append(1)
+        for each in range(numRows-1) : 
+            tempList = []
+            tempList.append(1) #FIRST1
+            
+            for i in range(each) :
+           #   0,1           2
+                prev_list = listlist[each] #1 1+1 1 PREVIOUS LIST RATHER THAN TEMPLIST 
+               #                      2
+                
+                tempList.append(prev_list[i] + prev_list[i+1])
 
-            #1 1
-            #1 1+1 1
-            #1 1+2 2+1 1
+            tempList.append(1) #LAST1
+            
             listlist.append(tempList)
 
-        print(listlist)        
+        return listlist        
             
             
 
