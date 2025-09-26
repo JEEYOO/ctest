0f3411a4aceb9fce2a526f22f14dcdfe0b14adfc
Jee Yoo <yoojw94@gmail.com>
2025-09-26 20:20:23 +0900

    Update maxProfit.py

--- leetCode/maxProfit.py
+++ leetCode/maxProfit.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+
+        minPrice = prices[0]
         answer = 0
 
         for i in range(len(prices)) : 
             stdPrice = prices[i]
-            maxP = max(prices[i:])
             
-            if answer < maxP - stdPrice :
-                answer = maxP - stdPrice    
+            if minPrice > stdPrice :
+                minPrice = stdPrice
+            else : 
+                answer = max(answer, stdPrice - minPrice)    
         
         return answer
