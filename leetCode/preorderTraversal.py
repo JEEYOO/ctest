a4564d7df79ddff1d0a09d2f8c80112222f2c216
Jee Yoo <yoojw94@gmail.com>
2025-10-01 16:34:32 +0900

    Update preorderTraversal.py

--- leetCode/preorderTraversal.py
+++ leetCode/preorderTraversal.py
@@ -2,15 +2,15 @@ def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
         resultList = [] 
 
-        def addPreOrderVal(root: Optional[TreeNode], rList: List[int]) -> List[int]:
-            if root is None : return rList
-
-            rList.append(root.val)
-            addPreOrderVal(root.left, rList)
-            addPreOrderVal(root.right, rList)
-
-            return rList
-
-        resultList = addPreOrderVal(root,resultList)
+        resultList = self.addPreOrderVal(root,resultList)
 
         return resultList
+    
+    def addPreOrderVal(self, root: Optional[TreeNode], rList: List[int]) -> List[int]:
+        if root is None : return rList
+
+        rList.append(root.val)
+        self.addPreOrderVal(root.left, rList)
+        self.addPreOrderVal(root.right, rList)
+
+        return rList
