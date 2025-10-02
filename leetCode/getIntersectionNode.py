fcfd670860e34530932f0e6652e792b0996e1986
Jee Yoo <yoojw94@gmail.com>
2025-10-02 18:46:22 +0900

    Update getIntersectionNode.py

--- leetCode/getIntersectionNode.py
+++ leetCode/getIntersectionNode.py
@@ -2,9 +2,10 @@ def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[List
     
         #SELECT ONE(A|B) AND STORE IN IN A LIST 
         listA = []
-        listA.append(headA.val)
+        
         while headA is not None and headA.next is not None : 
-            listA.append(headA.next)
+            listA.append(headA.val)
+            headA = headA.next
         
         #USE 'IN' TO CHECK FOR THE OTHER LIST AND RETURN IF FOUND
         while headB is not None and headB.next is not None : 
